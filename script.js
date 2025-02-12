137f0620e941d453930bceeac0f07b50af104e4f
Simone Del Grosso <simdlg@outlook.it>
2025-02-12 18:05:41 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+window.onload = () => {
+      
+  const home = document.getElementById("home");
+  const about = document.getElementById("about");
+  const contact = document.getElementById("contact");
+  const navbar = document.getElementById("navbar");
+  const navbarButtons = document.querySelectorAll(".nav-btn");
+  const backButton = document.getElementById("scroll-back-btn");
+  
+  window.addEventListener("scroll", () => {
+    if (window.scrollY < (home.offsetTop + home.offsetHeight - 60)) {
+      backButton.style.display = "none";
+      navbar.style.position = "absolute";
+      navbar.style.backgroundColor = "#40404030"
+      navbarButtons.forEach((button) => {
+        button.classList.remove("active");
+      });
+      navbarButtons[0].classList.add("active");
+    } else if (window.scrollY >= (home.offsetTop + home.offsetHeight - 60) && window.scrollY < (about.offsetTop + about.offsetHeight - 60)) {
+      backButton.style.display = "flex";
+  navbar.style.position = "fixed";
+      navbar.style.backgroundColor = "#404040"
+      navbarButtons.forEach((button) => {
+        button.classList.remove("active");
+      });
+      navbarButtons[1].classList.add("active");
+  } else if (window.scrollY >= (about.offsetTop + about.offsetHeight - 60) && window.scrollY < (contact.offsetTop + contact.offsetHeight - 60)) {
+  navbarButtons.forEach((button) => {
+        button.classList.remove("active");
+      });
+      navbarButtons[2].classList.add("active");
+  }
+  });
+  
+  const today = new Date();
+  const birthDate = new Date("1998-02-01");
+  let age = today.getFullYear() - birthDate.getFullYear();
+  let m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+  }
+  document.getElementById("about-age").innerText = age;
+  document.getElementById("footer-year").innerText = today.getFullYear();
+};
